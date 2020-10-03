83b681bd609f44cacfe2adb92e51b159af6ef1a8
Simon Cha <scha7@jh.edu>
2020-10-02 21:13:36 -0400

    added drop create table

--- homeworks/hw3/MyBooksApp/src/test/java/RESTAPITest.java
+++ homeworks/hw3/MyBooksApp/src/test/java/RESTAPITest.java
@@ -1,16 +1,43 @@
 import okhttp3.*;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import java.io.IOException;
 import java.sql.*;
+import java.sql.Connection;
+
 import static org.junit.Assert.*;
 
 public class RESTAPITest {
 
     static OkHttpClient client;
+
+    static String URI;
+    static Connection conn;
+    static Statement st;
+
     @BeforeClass
     public static void beforeClassTests() throws SQLException {
         client = new OkHttpClient();
+
+        URI = "jdbc:sqlite:./MyBooksApp.db";
+        conn = DriverManager.getConnection(URI);
+        st = conn.createStatement();
+
+        String sql = "DROP TABLE IF EXISTS Authors";
+        st.execute(sql);
+        sql = "DROP TABLE IF EXISTS Books";
+        st.execute(sql);
+
+        sql = "CREATE TABLE IF NOT EXISTS Authors (id INTEGER PRIMARY KEY, name VARCHAR(100) NOT NULL UNIQUE," +
+                " numOfBooks INTEGER, nationality VARCHAR(30));";
+        st.execute(sql);
+
+        sql = "CREATE TABLE IF NOT EXISTS Books (id INTEGER PRIMARY KEY, title VARCHAR(200) NOT NULL," +
+                " isbn VARCHAR(14) NOT NULL UNIQUE, publisher VARCHAR(14), year INTEGER," +
+                " authorId INTEGER, FOREIGN KEY(authorId) REFERENCES Authors(id)" +
+                " ON UPDATE CASCADE ON DELETE CASCADE);";
+        st.execute(sql);
     }
 
     @Test
@@ -38,4 +65,14 @@ public class RESTAPITest {
     }
 
     // TODO: Add your tests here
+//    @Test
+//    public void testListBooks() throws IOException {
+//        Request request = new Request.Builder()
+//                .url("http://localhost:7000/books")
+//                .build();
+//        Response response = client.newCall(request).execute();
+//        assertEquals(200, response.code());
+//    }
+
+
 }
