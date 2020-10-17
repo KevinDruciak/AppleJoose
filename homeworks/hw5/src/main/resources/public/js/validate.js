2a7818d56148d49b70602c919bc9f07c31938093
KevinDruciak <kevintdruciak@gmail.com>
2020-10-17 01:11:15 -0400

    validation done

--- homeworks/hw5/src/main/resources/public/js/validate.js
+++ homeworks/hw5/src/main/resources/public/js/validate.js
@@ -8,3 +8,32 @@ function validateUsername() {
     }
 }
 
+function validateAuthor() {
+    let name = document.getElementById("name");
+    let nameRegex = new RegExp("[a-zA-Z]*\\s[a-zA-Z]*");
+    if (!nameRegex.test(name.value)) {
+        alert("Not a valid name!");
+        return false;
+    }
+    return true;
+}
+
+function validateBook() {
+    let isbn = document.getElementById("isbn");
+    let isbnRegex = new RegExp("[0-9]{10}|[0-9]{3}[-][0-9]{10}");
+    if (!isbnRegex.test(isbn.value)) {
+        alert("Not a valid ISBN!");
+        return false;
+    }
+
+    let title = document.getElementById("title");
+    if (title.value.length < 1) {
+        alert("Book title cannot be empty!");
+        return false;
+    }
+
+    return true;
+}
+
+
+
