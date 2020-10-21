4c8443e2553d4995e209f0ee314d5fa64dd58731
Connor Devlin <71162207+ConnorDevlin2000@users.noreply.github.com>
2020-10-20 22:44:14 -0400

    Delete popup.js

--- AppleJuice/src/main/javaScript/model/popup.js
+++
@@ -1,5 +0,0 @@
-let checkPageButton = document.getElementById('checkPage');
-
-checkPageButton.addEventListener('click', function() {
-    chrome.tabs.create({url: "localhost:7000"}, {});
-    }, true);
