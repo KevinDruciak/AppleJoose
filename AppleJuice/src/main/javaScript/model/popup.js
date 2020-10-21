a9a2a841fe5bada7f98afb0ff590bc0bb524e6bf
Connor <connor.devlin@mac.com>
2020-10-20 22:41:29 -0400

    Implemented ChromeExtension. The extension currently counts the number of occurrences of the word 'bear' on a webpage. Implemented this to get better understanding of how Chrome extension works, and how to set it up.

--- AppleJuice/src/main/javaScript/model/popup.js
+++ AppleJuice/src/main/javaScript/model/popup.js
@@ -1,5 +1,22 @@
-let checkPageButton = document.getElementById('checkPage');
+document.addEventListener("DOMContentLoaded", function(){
 
-checkPageButton.addEventListener('click', function() {
-    chrome.tabs.create({url: "localhost:7000"}, {});
-    }, true);
+   document.querySelector("button").addEventListener("click", onclick, false);
+   function onclick(){
+
+      chrome.tabs.query({currentWindow: true, active: true},
+          function(tabs){
+
+            chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount);
+
+          })
+
+   }
+
+   function setCount(res){
+
+      const div = document.createElement('div');
+      div.textContent = `${res.count} bears`;
+      document.body.appendChild(div);
+
+   }
+}, false);
